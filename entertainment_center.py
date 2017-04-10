46dd0b10ff0b72d160d3f596ef03b2812a09e036
Obinna Ojialor <Obinna Ojialor>
2017-04-10 14:00:42 -0400

    adding more movies to entertainment

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,20 +2,35 @@ import media
 import fresh_tomatoes
 
 
-godzilla = media.Movie("Godzilla","urban", "http://www.planwallpaper.com/static/images/canberra_hero_image_JiMVvYU.jpg", "https://www.youtube.com/watch?v=vIu85WQTPRc")
+godzilla = media.Movie("Godzilla","urban",
+                       "http://www.planwallpaper.com/static/images/canberra_hero_image_JiMVvYU.jpg",
+                       "https://www.youtube.com/watch?v=vIu85WQTPRc")
 
-#print (godzilla.poster)
 
-avatar = media.Movie("Avatar","A story about Bishop","https://www.w3schools.com/css/trolltunga.jpg", "https://www.youtube.com/watch?v=helEv0kGHd4")
-#print (avatar.trailer_youtube_url)
 
-#avatar.show_trailer()
+avatar = media.Movie("Avatar","A story about Bishop",
+                     "https://www.w3schools.com/css/trolltunga.jpg",
+                     "https://www.youtube.com/watch?v=helEv0kGHd4")
 
-jezebels = media.Movie("The Jezebels", " A story about nigerian love", "http://www.planwallpaper.com/static/images/tree-247122.jpg", "https://www.youtube.com/watch?v=I8EOrcZ00rE")
 
-black_apple = media.Movie("Black Apple", "A story about wicked men", "http://www.planwallpaper.com/static/images/nasas-images-of-most-remarkable-events-you-cant-miss.jpg", "https://www.youtube.com/watch?v=aTo9nhoPlAE")
 
-#jezebels.show_trailer()
+jezebels = media.Movie("The Jezebels", " A story about nigerian love",
+                       "http://www.planwallpaper.com/static/images/tree-247122.jpg",
+                       "https://www.youtube.com/watch?v=I8EOrcZ00rE")
+
+black_apple = media.Movie("Black Apple", "A story about wicked men",
+                          "http://www.planwallpaper.com/static/images/nasas-images-of-most-remarkable-events-you-cant-miss.jpg",
+                          "https://www.youtube.com/watch?v=aTo9nhoPlAE")
+
+black_apple = media.Movie("Black Apple", "A story about wicked men",
+                          "http://www.planwallpaper.com/static/images/nasas-images-of-most-remarkable-events-you-cant-miss.jpg",
+                          "https://www.youtube.com/watch?v=aTo9nhoPlAE")
+
+black_apple = media.Movie("Black Apple", "A story about wicked men",
+                          "http://www.planwallpaper.com/static/images/nasas-images-of-most-remarkable-events-you-cant-miss.jpg",
+                          "https://www.youtube.com/watch?v=aTo9nhoPlAE"
+
+
 movies = [godzilla, avatar,jezebels,black_apple]
 fresh_tomatoes.open_movies_page(movies)
 
