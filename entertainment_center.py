3565905f1b13c3a79ccda4580723c32c3b07db38
Obinna Ojialor <Obinna Ojialor>
2017-04-12 00:28:39 -0400

    Merged changes from phase one

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,26 +3,37 @@ import fresh_tomatoes
 
 
 godzilla = media.Movie("Godzilla","urban",
-                       "http://www.planwallpaper.com/static/images/canberra_hero_image_JiMVvYU.jpg",
-                       "https://www.youtube.com/watch?v=vIu85WQTPRc")
-
+                       "https://pmchollywoodlife.files.wordpress.com/2014/05/godzilla-poster-lead.jpg?w=600",
+                       "https://www.youtube.com/watch?v=vIu85WQTPRc&t=2s")
 
 
 avatar = media.Movie("Avatar","A story about Bishop",
-                     "https://www.w3schools.com/css/trolltunga.jpg",
-                     "https://www.youtube.com/watch?v=helEv0kGHd4")
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 
-
-jezebels = media.Movie("The Jezebels", " A story about nigerian love",
-                       "http://www.planwallpaper.com/static/images/tree-247122.jpg",
+jezebels = media.Movie("The Jezebels", "A story about nigerian love",
+                       "https://i.ytimg.com/vi/I8EOrcZ00rE/maxresdefault.jpg",
                        "https://www.youtube.com/watch?v=I8EOrcZ00rE")
+
 
 black_apple = media.Movie("Black Apple", "A story about wicked men",
                           "http://www.planwallpaper.com/static/images/nasas-images-of-most-remarkable-events-you-cant-miss.jpg",
                           "https://www.youtube.com/watch?v=aTo9nhoPlAE")
 
+instagram_girls = media.Movie("Instragram Girls", "A story about escort girls", 
+                              "https://i.ytimg.com/vi/eHWC08Re_C8/maxresdefault.jpg",
+                              "https://www.youtube.com/watch?v=oA3PeXQtXuk")
 
-movies = [godzilla, avatar,jezebels,black_apple]
+single_ladies = media.Movie("Single ladies", "A story about extravagant ladies",
+                            "https://i.ytimg.com/vi/xjr-1DH3xA4/maxresdefault.jpg,
+                            "https://www.youtube.com/watch?v=24Dc67ZQBug")
+
+undercover_lover = media.Movie("Undercover Lover", "A story about love",
+                               "https://i.ytimg.com/vi/XzJJBeAVZVE/maxresdefault.jpg",
+                               "https://www.youtube.com/watch?v=3Vj94AB5gtE")
+
+
+movies = [godzilla, avatar,jezebels,black_apple,instagram_girls,single ladies,undercover_lover]
 fresh_tomatoes.open_movies_page(movies)
 
